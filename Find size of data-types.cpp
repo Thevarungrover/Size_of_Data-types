18f1680f71cfcf2ccfcb7c7727b877b35ff1ade7
Varun Grover <69295765+Thevarungrover@users.noreply.github.com>
2020-10-04 04:26:46 -0700

    added file

---
+++ Find size of data-types.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+main()
+{
+//	'SIZEOF()' --> TO FIND SIZE
+	int a;
+	char b;
+	float c;
+	double d;
+	cout<<"\nSize of char : "<<sizeof(b)<<" bytes";
+	cout<<"\nSize of int : "<<sizeof(a)<<" bytes";
+	cout<<"\nSize of float : "<<sizeof(c)<<" bytes";
+	cout<<"\nSize of double : "<<sizeof(d)<<" bytes";
+}
